4c77d323bee35f9cfa65d56cc1e2a04bac898e10
rajshiv169 <rajshiv169@gmail.com>
2019-01-15 23:35:23 +0530

    started papers template

--- home/views.py
+++ home/views.py
@@ -2,4 +2,13 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, template_name='index.html')
+    context = {
+        'page_title' : 'Home',
+    }
+    return render(request, template_name='index.html', context=context)
+
+def papers(request):
+    context = {
+        'page_title' : 'Papers',
+    }
+    return render(request, template_name='papers.html', context=context)
